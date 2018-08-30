e6b9cf85353278595006709888a5068031153cd3
Adam Mukharil Bachtiar <adammbachtiar@gmail.com>
2018-08-30 15:51:22 +0700

    init project MySimpleMVPPattern

---
+++ app/src/main/java/com/example/adammb/mysimplemvppattern/MainPresenter.java
@@ -0,0 +1,20 @@
+package com.example.adammb.mysimplemvppattern;
+
+public class MainPresenter {
+    private MainView view;
+
+    public MainPresenter(MainView view) {
+        this.view = view;
+    }
+
+    public double volume(double panjang, double lebar,double tinggi){
+        return panjang*lebar*tinggi;
+    }
+
+    public void hitungVolume(double panjang, double lebar, double tinggi){
+        double volume =volume(panjang,lebar,tinggi);
+
+        MainModel model=new MainModel(volume);
+        view.tampilVolume(model);
+    }
+}
---
+++ app/src/main/java/com/example/adammb/mysimplemvppattern/MainView.java
@@ -0,0 +1,5 @@
+package com.example.adammb.mysimplemvppattern;
+
+public interface MainView {
+    void tampilVolume(MainModel model);
+}
